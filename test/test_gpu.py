f93853f8d086fd8f4f4a81c5855db5dc14b1396d
u5977862 <user.name>
2025-08-08 14:55:58 +0800

    Stop tracking test/

--- test/test_gpu.py
+++
@@ -1,9 +0,0 @@
-import torch as th
-
-if th.cuda.is_available():
-    gpu_count = th.cuda.device_count()
-    print(f"偵測到 {gpu_count} 個可用的 GPU。")
-    for i in range(gpu_count):
-        print(f"  GPU {i}: {th.cuda.get_device_name(i)}")
-else:
-    print("未偵測到任何可用的 GPU。")
